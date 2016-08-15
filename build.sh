3ba275e4b0e3bf6742872f072b9c55aba0ff1aab
unknown <jameslovejoy1@gmail.com>
2016-08-15 15:34:29 -0400

    Add missing libraries.

--- build.sh
+++ build.sh
@@ -9,4 +9,4 @@ exe ${CXX} ${CXXFLAGS} -c main.cpp -o main.o
 exe ${CXX} ${CXXFLAGS} -c protocol.cpp -o protocol.o
 exe ${CXX} ${CXXFLAGS} -c wallet.cpp -o wallet.o
 exe ${CXX} ${CXXFLAGS} -c rpcserver.cpp -o rpcserver.o
-exe ${CXX} main.o protocol.o wallet.o rpcserver.o -o CryptoCurrency -lCryptoKernel -ljsoncpp -lcrypto -lleveldb -lenet -ljsonrpccpp
+exe ${CXX} main.o protocol.o wallet.o rpcserver.o -o CryptoCurrency -lCryptoKernel -ljsoncpp -pthread -lenet -lcrypto -lleveldb -lmicrohttpd -ljsonrpccpp-common -ljsonrpccpp-server -lcurl -ljsonrpccpp-client -ldl
