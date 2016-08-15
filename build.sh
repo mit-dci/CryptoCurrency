e24d4b0ef13de7a5455205394030e32428dbf2d8
unknown <jameslovejoy1@gmail.com>
2016-08-15 15:26:51 -0400

    Add missing library and fix spelling mistake.

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ CXX=g++
 CXXFLAGS="-Wall -std=c++14 -O2"
 
 exe ${CXX} ${CXXFLAGS} -c main.cpp -o main.o
-exe ${CXX} ${CXXFLAGS} -c protocl.cpp -o protocol.o
+exe ${CXX} ${CXXFLAGS} -c protocol.cpp -o protocol.o
 exe ${CXX} ${CXXFLAGS} -c wallet.cpp -o wallet.o
 exe ${CXX} ${CXXFLAGS} -c rpcserver.cpp -o rpcserver.o
-exe ${CXX} base64.o main.o protocol.o wallet.o rpcserver.o -o CryptoCurrency -lCryptoKernel -ljsoncpp -lcrypto -lleveldb -lenet
+exe ${CXX} base64.o main.o protocol.o wallet.o rpcserver.o -o CryptoCurrency -lCryptoKernel -ljsoncpp -lcrypto -lleveldb -lenet -ljsonrpccpp
